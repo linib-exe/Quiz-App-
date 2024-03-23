8e9d8b37e928291473326cecf5faf49153e8e90e
linib.exe <linib.exe@gmail.com>
2024-03-23 17:28:55 +0545

    Quiz Updated

--- quizapp/views.py
+++ quizapp/views.py
@@ -3,19 +3,15 @@ from django.contrib import messages
 from .models import Question, Choice
 
 def home(request):
+    questions = Question.objects.all()
     if request.method == 'POST':
         score = 0
         total_questions = Question.objects.count()
-        
-        for question in Question.objects.all():
+        for question in questions:
             selected_choice_id = request.POST.get(f'question{question.id}')
             correct_choice = Choice.objects.get(question=question, is_correct=True)
-            
-            if selected_choice_id and int(selected_choice_id) == correct_choice.id:
+            if int(selected_choice_id) == correct_choice.id:
                 score += 1
-        
         messages.success(request, f'You scored {score} out of {total_questions}')
         return redirect('home')
-
-    questions = Question.objects.all()
     return render(request, 'home.html', {'questions': questions})
