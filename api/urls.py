1ed98a80c122a5364daa2dfac1002a0fdb121afb
linib.exe <linib.exe@gmail.com>
2024-03-24 08:25:07 +0545

    CRUD updated

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path('get-quiz/',views.get_quiz,name='get-quiz'),
     path('get-choices/',views.get_choices,name='get-choices'),
     path('create-questions/',views.create_questions,name='create-questions'),
+    path('create-quiz/',views.create_quiz,name='create-quiz'),
+    path('create-choices/',views.create_choices,name='create-choices'),
+    path('update-question/<int:id>',views.update_question,name='update-question'),
+    path('update-quiz/<int:id>',views.update_quiz,name='update-quiz'),
+    path('update-choice/<int:id>',views.update_choice,name='update-choice'),
+    path('delete-question/<int:id>',views.delete_question,name='delete-question'),
 ]
--- quizapp/views.py
+++ quizapp/views.py
@@ -23,6 +23,65 @@ def home(request):
 
 
 #-----------rest-framework-----------------------------------------------------------
+#-------------------------CREATE-----------------------------------------------------
+@api_view(['POST'])
+def create_questions(request):
+    data = request.data
+    serializer = QuestionSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(
+            {
+                'status':status.HTTP_200_OK,
+                'data':serializer.data
+            }
+        )
+    else:
+        return Response(
+            {
+                'message':"Bad request"
+            }
+        )
+
+@api_view(['POST'])   
+def create_quiz(request):
+    data = request.data
+    serializer = QuizSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(
+            {
+                'status':status.HTTP_200_OK,
+                'data':serializer.data
+            }
+        )
+    else:
+        return Response(
+            {
+                'message':"Bad Request"
+            }
+        )
+
+@api_view(['POST'])    
+def create_choices(request):
+    data = request.data
+    serializer = ChoiceSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(
+            {
+                'status':status.HTTP_200_OK,
+                'data':serializer.data
+            }
+        )
+    else:
+        return Response(
+            {
+                'message':'Bad Request'
+            }
+        )
+
+#----------------------------------READ-------------------------------------------------
 @api_view(['GET'])
 def get_questions(request):
     questions = Question.objects.all()
@@ -56,22 +115,100 @@ def get_choices(request):
         }
     )
 
-@api_view(['POST'])
-def create_questions(request):
-    data = request.data
-    serializer = QuestionSerializer(data=data)
+#----------------------------UPDATE--------------------------------------------------------
+@api_view(['PUT','PATCH'])
+def update_question(request,id):
+    question  = Question.objects.get(id =id)
+    serializer = QuestionSerializer(question,data=request.data,partial=True)
     if serializer.is_valid():
         serializer.save()
         return Response(
             {
+                'data':serializer.data,
                 'status':status.HTTP_200_OK,
-                'data':serializer.data
+                'message':'Data Updated Successfully'
             }
         )
     else:
         return Response(
             {
-                'message':"Bad request"
+                'message':'Error updating question',
+                'status':status.HTTP_400_BAD_REQUEST,
+                'errors':serializer.errors
             }
         )
 
+@api_view(['PUT','PATCH'])
+def update_quiz(request,id):
+    quiz = Quiz.objects.get(id=id)
+    data = request.data
+    serializer = QuizSerializer(quiz,data=data,partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(
+            {
+                'message':'Data Updated Successfully',
+                'data':serializer.data,
+                'status':status.HTTP_200_OK
+            }
+        )
+    else:
+        return Response(
+            {
+                'message':'Error updating quiz',
+                'status':status.HTTP_400_BAD_REQUEST,
+            }
+        )
+
+@api_view(['PUT','PATCH'])
+def update_choice(request,id):
+    choice = Choice.objects.get(id=id)
+    data = request.data
+    serializer = QuizSerializer(choice,data=data,partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(
+            {
+                'message':'Data Updated Successfully',
+                'data':serializer.data,
+                'status':status.HTTP_200_OK
+            }
+        )
+    else:
+        return Response(
+            {
+                'message':'Error updating choice',
+                'status':status.HTTP_400_BAD_REQUEST,
+            }
+        )
+
+#------------------------DELETE------------------------------------------
+@api_view(['DELETE'])
+def delete_question(request,id):
+    question = Question.objects.get(id=id)
+    question.delete()
+    return Response(
+        {
+            'message':'Data deleted sucessfully'
+        }
+    )
+
+@api_view(['DELETE'])
+def delete_quiz(request,id):
+    quiz = Quiz.objects.get(id=id)
+    quiz.delete()
+    return Response(
+        {
+            'message':'Data deleted sucessfully'
+        }
+    )g
+
+@api_view(['DELETE'])
+def delete_choice(request,id):
+    choice = Choice.objects.get(id=id)
+    choice.delete()
+    return Response(
+        {
+            'message':'Data deleted sucessfully'
+        }
+    )
